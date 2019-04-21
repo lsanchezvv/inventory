4f7dca36f801b939166dea76bc253f41c163657e
lsanchezvv <lsanchez@vivial.net>
2019-04-21 14:11:15 -0400

    Creating more controllers

---
+++ api/controllers/item-controller.js
@@ -0,0 +1,26 @@
+'use strict'
+
+async function create (req, res) {
+
+}
+
+async function get (req, res) {
+
+}
+
+async function getAll (req, res) {
+
+}
+
+async function update (req, res) {
+
+}
+
+const API = {
+  create,
+  get,
+  getAll,
+  update
+}
+
+module.exports = API
