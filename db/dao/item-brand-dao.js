20f24dd4b928a9a28b552ad7989b3386af372e70
lsanchezvv <lsanchez@vivial.net>
2019-05-16 20:50:42 -0400

    Working on item brand dao

--- db/dao/item-brand-dao.js
+++ db/dao/item-brand-dao.js
@@ -1,17 +1,25 @@
 'use strict'
 
-const db = require('../db')
+const { getDatabase } = require('../db')
 
-async function create () {
-
+async function create (itemBrand, db = getDatabase()) {
+  return db.table('item_brand').insert(itemBrand).returning('id')
 }
 
-async function update () {
-
+async function update (itemBrandId, itemBrand, db = getDatabase()) {
+  return db.table('item_brand')
+    .update(itemBrand)
+    .where('id', itemBrandId)
+    .returning('id')
 }
 
-async function getById () {
+async function getById (itemBrandId, db = getDatabase()) {
+  const [itemBrand] = await db.table('item_brand').select().where('id', itemBrandId)
+  return itemBrand
+}
 
+async function getAll (db = getDatabase()) {
+  return db.table('item_brand').select()
 }
 
 async function deleteBrand () {
@@ -19,7 +27,10 @@ async function deleteBrand () {
 }
 
 const API = {
-
+  create,
+  update,
+  getById,
+  getAll
 }
 
 module.exports = API
