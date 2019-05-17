c88cf6d8544ad2d3ea78d2fd17537bbe5f62e815
lsanchezvv <lsanchez@vivial.net>
2019-05-16 20:54:17 -0400

    Working on item type table

--- db/dao/item-type.js
+++ db/dao/item-type.js
@@ -1,25 +1,34 @@
 'use strict'
 
-const db = require('../db')
+const { getDatabase } = require('../db')
 
-async function create () {
+async function create (itemType, db = getDatabase()) {
+  return db.table('item_type').insert(itemType).returning('id')
+}
+
+async function update (itemTypeId, itemType, db = getDatabase()) {
+  return db.table('item_type')
+    .update(itemType)
+    .where('id', itemTypeId)
+    .returning('id')
 
 }
 
-async function update () {
+async function getById (itemTypeId, db = getDatabase()) {
+  const [itemType] = await db.table('item_type').select().where('id', itemTypeId)
+  return itemType
 
 }
 
-async function getById () {
-
-}
-
-async function deleteType () {
-
+async function getAll (db = getDatabase()) {
+  return db.table('item_type').select()
 }
 
 const API = {
-
+  create,
+  update,
+  getById,
+  getAll
 }
 
 module.exports = API
