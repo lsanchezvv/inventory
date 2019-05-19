756b0451253bb0b8b0e4de2323f4801073c9e76a
lsanchezvv <lsanchez@vivial.net>
2019-05-19 18:53:29 -0400

    Adding Status dao

--- db/dao/status-dao.js
+++ db/dao/status-dao.js
@@ -1,21 +1,31 @@
 'use strict'
 
-const db = require('../db')
+const { getDatabase } = require('../db')
 
-async function create () {
-
+async function create (status, db = getDatabase()) {
+  return db.getDatabase('status').insret(status).returning('id')
 }
 
-async function update () {
-
+async function update (statusId, status, db = getDatabase()) {
+  return db.table('status')
+    .update(status)
+    .where('id', statusId)
+    .returning('id')
 }
 
-async function getById () {
-
+async function getById (statusId, db = getDatabase()) {
+  const [status] = await db.table('status').select().where('id', statusId)
+  return status
 }
 
-
+async function getAll (db = getDatabase()) {
+  return db.table('status').select()
+}
 const API = {
+  create,
+  update,
+  getById,
+  getAll
 
 }
 
